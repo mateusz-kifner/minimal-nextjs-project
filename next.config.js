ebf10ace0b6484454e9a521805ef203fb4d0451b
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2023-08-23 19:09:26 +0200

    Added logging and changed node version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify:false,
+  webpack:(config)=>
+  {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+return config
+  }
 }
 
 module.exports = nextConfig
